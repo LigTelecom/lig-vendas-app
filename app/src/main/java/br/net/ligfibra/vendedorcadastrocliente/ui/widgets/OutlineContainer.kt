f23aaa34b34a820fe9839881d4f58a3226a37d29
LSaints <lsants.dev@gmail.com>
2025-06-13 13:01:24 -0300

    (feat): adicionar classe para manipular configurações de cor do OutlineContainer.kt

--- app/src/main/java/br/net/ligfibra/vendedorcadastrocliente/ui/widgets/OutlineContainer.kt
+++ app/src/main/java/br/net/ligfibra/vendedorcadastrocliente/ui/widgets/OutlineContainer.kt
@@ -1,32 +1,48 @@
 package br.net.ligfibra.vendedorcadastrocliente.ui.widgets
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.net.ligfibra.vendedorcadastrocliente.ui.theme.VendedorcadastroclienteTheme
 
+data class OutlinedContainerColors(
+    val borderColor: Color,
+    val backgroundColor: Color,
+)
+
 @Composable
-fun OutlineContainer(content: @Composable () -> Unit) {
+fun OutlineContainer(
+    outlinedContainerColors: OutlinedContainerColors = OutlinedContainerColors(
+        borderColor = MaterialTheme.colorScheme.tertiary,
+        backgroundColor = Color.Transparent,
+    ),
+    content: @Composable () -> Unit
+) {
     Column(
         modifier = Modifier
             .border(
                 border = BorderStroke(
                     width = 0.4.dp,
-                    color = MaterialTheme.colorScheme.tertiary
+                    color = outlinedContainerColors.borderColor
                 ),
                 shape = RoundedCornerShape(size = 12.dp)
+
             )
             .clip(RoundedCornerShape(size = 12.dp))
+            .background(outlinedContainerColors.backgroundColor)
             .padding(horizontal = 12.dp, vertical = 12.dp)
             .fillMaxWidth()
     ) {
@@ -38,8 +54,10 @@ fun OutlineContainer(content: @Composable () -> Unit) {
 @Composable
 private fun OutlineContainerPreview() {
     VendedorcadastroclienteTheme {
-        OutlineContainer {
-            Text("teste")
+        Surface {
+            OutlineContainer {
+                Text("teste")
+            }
         }
     }
 }
