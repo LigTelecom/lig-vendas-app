41d219e74903864b4fcc8c155e89e030d2c9693c
LSaints <lsants.dev@gmail.com>
2025-06-13 13:09:15 -0300

    (feat): adicionar construtor para ClienteContato.kt onde permite criar uma instâcia com apenas o telefone

--- app/src/main/java/br/net/ligfibra/vendedorcadastrocliente/core/entities/ClienteContato.kt
+++ app/src/main/java/br/net/ligfibra/vendedorcadastrocliente/core/entities/ClienteContato.kt
@@ -5,7 +5,17 @@ import br.net.ligfibra.vendedorcadastrocliente.core.exceptions.cliente.EmailInva
 
 class ClienteContato(val telefone: String, val email: String) {
     init {
-        require(email.matches(Regex("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"))) { throw EmailInvalidoException() }
-        require(telefone.matches(Regex("\\d{10,11}"))) { throw ClienteTelefoneInvalidoException() }
+        require(email.matches(Regex("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"))) {
+            throw EmailInvalidoException()
+        }
+        require(telefone.matches(Regex("\\d{10,11}"))) {
+            throw ClienteTelefoneInvalidoException()
+        }
+    }
+
+    constructor(telefone: String) : this(telefone = telefone, email = "") {
+        require(telefone.matches(Regex("\\d{10,11}"))) {
+            throw ClienteTelefoneInvalidoException()
+        }
     }
 }
