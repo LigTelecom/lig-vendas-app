806c400fc1609703c5e76de5800fc0cb53f7d63d
LSaints <lsants.dev@gmail.com>
2025-06-13 13:00:27 -0300

    (feat): adicionar verificação de permissão de acesso a câmera

--- app/src/main/java/br/net/ligfibra/vendedorcadastrocliente/MainActivity.kt
+++ app/src/main/java/br/net/ligfibra/vendedorcadastrocliente/MainActivity.kt
@@ -7,19 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
-
 import androidx.navigation.compose.rememberNavController
 import br.net.ligfibra.vendedorcadastrocliente.ui.navigation.AppNavGraph
-
-
 import br.net.ligfibra.vendedorcadastrocliente.ui.theme.VendedorcadastroclienteTheme
 
+
+var shouldShowCamera: MutableState<Boolean> = mutableStateOf<Boolean>(value = false)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
