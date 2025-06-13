c6e1d4be9e66427de97d277b374fb7d24aa56e13
LSaints <lsants.dev@gmail.com>
2025-06-13 13:11:59 -0300

    (feat): implementar e envolver formularios para cadastro de clientes em um único cliente

--- app/src/main/java/br/net/ligfibra/vendedorcadastrocliente/ui/screens/cadastrocliente/CadastrarClienteScreen.kt
+++ app/src/main/java/br/net/ligfibra/vendedorcadastrocliente/ui/screens/cadastrocliente/CadastrarClienteScreen.kt
@@ -1,34 +1,102 @@
 package br.net.ligfibra.vendedorcadastrocliente.ui.screens.cadastrocliente
 
+import android.graphics.Bitmap
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.net.ligfibra.vendedorcadastrocliente.ui.screens.cadastrocliente.forms.ClientAdressForm
+import br.net.ligfibra.vendedorcadastrocliente.ui.screens.cadastrocliente.forms.ClientInfoForm
+import br.net.ligfibra.vendedorcadastrocliente.ui.screens.cadastrocliente.forms.DocumentsUploadForm
+import br.net.ligfibra.vendedorcadastrocliente.ui.theme.VendedorcadastroclienteTheme
+import br.net.ligfibra.vendedorcadastrocliente.ui.utils.Form
+import br.net.ligfibra.vendedorcadastrocliente.ui.widgets.GetPermissions
 
 @Composable
 fun CadastrarClienteScreen(modifier: Modifier = Modifier) {
-    Column {
+    var step by remember { mutableStateOf<Int>(1) }
+
+    val context = LocalContext.current
+
+    var imgRG by remember { mutableStateOf<Bitmap?>(null) }
+    var imgComprovanteResidencia by remember { mutableStateOf<Bitmap?>(null) }
+
+    fun nextStep(): Unit {
+        step += 1
+    }
+
+    fun backStep(): Unit {
+        step -= 1
+    }
+
+    val FormsArray: List<Form> = listOf(
+        Form(
+            step = 1,
+            content = {
+                DocumentsUploadForm(::nextStep)
+            },
+        ),
+        Form(
+            step = 2,
+            content = {
+                ClientInfoForm(
+                    ::nextStep,
+                    ::backStep
+                )
+            },
+        ),
+        Form(
+            step = 3,
+            content = {
+                ClientAdressForm(
+                    ::nextStep,
+                    ::backStep
+                )
+            },
+        )
+    )
+
+    Column(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.tertiary),
+    ) {
+        GetPermissions()
         Column(
             modifier = modifier
                 .fillMaxSize()
-                .background(MaterialTheme.colorScheme.tertiary),
+                .padding(all = 16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "Cadastrar Cliente Page",
-                fontSize = 40.sp,
-                fontWeight = FontWeight.SemiBold,
-                color = Color.White
-            )
+            for (form in FormsArray) {
+                if (form.step == step) {
+                    form.content()
+                }
+            }
         }
+    }
+}
+
+fun anyImageIsNull(imgRG: Bitmap?, imgComprovanteResidencia: Bitmap?): Boolean {
+    return !(imgRG == null && imgComprovanteResidencia == null)
+}
+
+@Preview
+@Composable
+fun CadastrarClienteScreenPreview() {
+    VendedorcadastroclienteTheme {
+        CadastrarClienteScreen()
     }
 }
