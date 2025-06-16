2bbfa649e4ab2dbe0500fdac8684ab70beb70ed5
LSaints <lsants.dev@gmail.com>
2025-06-16 14:56:03 -0300

    (perm): adicionar permissões de localização

--- app/src/main/java/br/net/ligfibra/vendedorcadastrocliente/ui/widgets/GetPermissions.kt
+++ app/src/main/java/br/net/ligfibra/vendedorcadastrocliente/ui/widgets/GetPermissions.kt
@@ -5,8 +5,6 @@ import android.content.pm.PackageManager
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
@@ -20,6 +18,8 @@ fun GetPermissions() {
     ) { permissions ->
         val cameraGranted = permissions[Manifest.permission.CAMERA] ?: false
         val storageGranted = permissions[Manifest.permission.READ_EXTERNAL_STORAGE] ?: false
+        val fineLocationGranted = permissions[Manifest.permission.ACCESS_FINE_LOCATION] ?: false
+        val coarseLocation = permissions[Manifest.permission.ACCESS_COARSE_LOCATION] ?: false
 
         if (cameraGranted && storageGranted) {
             Log.d("PermissaoDebug", "Todas as permissões concedidas!")
@@ -33,7 +33,9 @@ fun GetPermissions() {
     LaunchedEffect(Unit) {
         val permissionsNeeded = arrayOf(
             Manifest.permission.CAMERA,
-            Manifest.permission.READ_EXTERNAL_STORAGE
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
         )
 
         val allGranted = permissionsNeeded.all {
