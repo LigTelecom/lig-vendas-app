e47de19bdf71d17640c39b915b3b1b42173a6d31
LSaints <lsants.dev@gmail.com>
2025-06-25 11:44:20 -0300

    (chore): adicionar inicialização da classe de consulta

--- app/src/main/java/br/net/ligfibra/vendedorcadastrocliente/services/requests/erp/Query.kt
+++ app/src/main/java/br/net/ligfibra/vendedorcadastrocliente/services/requests/erp/Query.kt
@@ -4,11 +4,11 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 class Query(
-    val qtype: String,
-    val query: String,
-    val oper: String,
-    val page: String,
-    val rp: String,
-    val sortname: String,
-    val sortoder: String
+    val qtype: String = "",
+    val query: String = "",
+    val oper: String = "",
+    val page: String = "",
+    val rp: String = "",
+    val sortname: String = "",
+    val sortoder: String = ""
 )
