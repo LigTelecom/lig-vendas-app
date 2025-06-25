6ee58c5751c47744030f2844016d109846d97fd9
LSaints <lsants.dev@gmail.com>
2025-06-25 12:18:13 -0300

    (chore): refatorar formulário de informações

--- app/src/main/java/br/net/ligfibra/vendedorcadastrocliente/ui/screens/cadastrocliente/forms/clientInfoForm/ClientInfoForm.kt
+++ app/src/main/java/br/net/ligfibra/vendedorcadastrocliente/ui/screens/cadastrocliente/forms/clientInfoForm/ClientInfoForm.kt
@@ -1,6 +1,7 @@
-package br.net.ligfibra.vendedorcadastrocliente.ui.screens.cadastrocliente.forms
+package br.net.ligfibra.vendedorcadastrocliente.ui.screens.cadastrocliente.forms.clientInfoForm
 
-import android.util.Log
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -40,7 +41,9 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.net.ligfibra.vendedorcadastrocliente.core.enums.EstadoCivilEnum
+import br.net.ligfibra.vendedorcadastrocliente.core.validation.ValidationResult
 import br.net.ligfibra.vendedorcadastrocliente.ui.theme.VendedorcadastroclienteTheme
+import br.net.ligfibra.vendedorcadastrocliente.ui.utils.extensions.CPFVisualTransformation
 import br.net.ligfibra.vendedorcadastrocliente.ui.widgets.HorizontalDividerWithText
 import br.net.ligfibra.vendedorcadastrocliente.ui.widgets.OutlineContainer
 import br.net.ligfibra.vendedorcadastrocliente.ui.widgets.OutlinedContainerColors
@@ -48,40 +51,65 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
+@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ClientInfoForm(
+    viewModel: ClientInfoFormsViewModel,
     next: () -> Unit,
     back: () -> Unit) {
 
-    var name by remember { mutableStateOf("") }
+    // Info Pessoal
+    var name by viewModel::nomeCliente
+    var nomeErroMessage by viewModel::nomeClienteErrorMessage
 
     val focusManager = LocalFocusManager.current
     var showDatePickerDialog by remember { mutableStateOf(false) }
     val datePickerState = rememberDatePickerState()
-    var selectedDate by remember { mutableStateOf("") }
 
-    var clientContactPhone by remember { mutableStateOf("") }
+    var selectedDate by viewModel::dataNascimento
+    var selectedDateErrorMessage by viewModel::dataNascimentoErrorMessage
+
     var estadoCivil by remember { mutableStateOf("") }
-    var clientEmail by remember { mutableStateOf("") }
-    var cpf by remember { mutableStateOf("") }
-    var rg by remember { mutableStateOf("") }
-    var orgaoEmissor by remember { mutableStateOf("") }
-    var naturalidade by remember { mutableStateOf("") }
+
+    val clientContactPhone by viewModel::telefone
+    val clientContactPhoneError by viewModel::telefoneErrorMessage
+
+    // Contato
+    var clientEmail by viewModel::email
+    var clientEmailErrorMessage by viewModel::emailErrorMessage
+
+    // Documentos
+    var cpf by viewModel::cpf
+    var cpfErrorMessage by viewModel::cpfErrorMessage
+    var rg by viewModel::rg
+    var rgErrorMessage by viewModel::rgErrorMessage
+
+    var orgaoEmissor by viewModel::orgaoEmissor
+    var orgaoEmissorErrorMessage by viewModel::orgaoEmissorErrorMessage
+
+    var naturalidade by viewModel::naturalidade
+    var naturalidadeErrorMessage by viewModel::naturalidadeErrorMessage
+
     var expandedEstadoCivilSelect = remember { mutableStateOf(false) }
 
     Column(verticalArrangement = Arrangement.Center) {
         HorizontalDividerWithText(text = "Informações pessoais")
         Row {
-            OutlinedTextField(
-                value = name, onValueChange = {
-                    name = it
-                },
-                modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth(),
-                label = { Text("*Nome Completo") },
-            )
+            Column {
+                OutlinedTextField(
+                    value = viewModel.nomeCliente,
+                    onValueChange = { viewModel.nomeCliente = it },
+                    label = { Text("*Nome Completo") },
+                    isError = nomeErroMessage.isNotEmpty(),
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                )
+                if (nomeErroMessage.isNotEmpty()) {
+                    Text(text = nomeErroMessage, color = Color.Red)
+                }
+            }
         }
         if (showDatePickerDialog) {
             DatePickerDialog(
@@ -105,22 +133,25 @@ fun ClientInfoForm(
         }
 
         Row{
-            OutlinedTextField(
-                value = selectedDate,
-                onValueChange = {},
-                modifier = Modifier
-                    .padding(8.dp)
-                    .onFocusEvent {
-                        if (it.isFocused) {
-                            showDatePickerDialog = true
-                            focusManager.clearFocus(force = true)
-                        }
-                    },
-                label = {
-                    Text("*Data de nascimento")
-                },
-                readOnly = true
-            )
+            Column {
+                OutlinedTextField(
+                    value = selectedDate,
+                    onValueChange = {},
+                    label = { Text("*Data de nascimento") },
+                    readOnly = true,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .onFocusEvent {
+                            if (it.isFocused) {
+                                showDatePickerDialog = true
+                                focusManager.clearFocus(force = true)
+                            }
+                        },
+                )
+                if (selectedDateErrorMessage.isNotEmpty()) {
+                    Text(text = selectedDateErrorMessage, color = Color.Red)
+                }
+            }
 
             val estadocivis = EstadoCivilEnum.getEstadoCivis()
             val estadoCivilSelected = remember {
@@ -144,11 +175,9 @@ fun ClientInfoForm(
                         verticalAlignment = Alignment.CenterVertically,
                         modifier = Modifier.fillMaxWidth()
                     ) {
-                        Text(text = "Estado Civil", color = MaterialTheme.colorScheme.secondary,)
+                        Text(text = "Estado Civil", color = MaterialTheme.colorScheme.secondary)
                         val arrowIcon = if (expandedEstadoCivilSelect.value != true) Icons.Default.KeyboardArrowDown else Icons.Default.KeyboardArrowUp
-                        Icon(
-                            imageVector = arrowIcon, contentDescription = null
-                        )
+                        Icon( imageVector = arrowIcon, contentDescription = null )
                     }
                     Text(
                         text = estadoCivilSelected.value.toString(),
@@ -174,85 +203,89 @@ fun ClientInfoForm(
         OutlinedTextField(
             value = naturalidade,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
-            onValueChange = {
-                naturalidade = it
-            },
+            label = { Text("*Naturalidade") },
+            onValueChange = { naturalidade = it },
             modifier = Modifier
                 .padding(8.dp),
-            label = {
-                Text("*Naturalidade")
-            }
         )
         HorizontalDividerWithText(text = "Informações para contato")
         Row {
-            OutlinedTextField(
-                value = clientContactPhone,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
-                onValueChange = {
-                    clientContactPhone = it
-                },
-                modifier = Modifier
-                    .padding(8.dp),
-                label = {
-                    Text("*Telefone")
+            Column {
+                OutlinedTextField(
+                    value = clientContactPhone,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+                    onValueChange = { viewModel.telefone = it },
+                    label = { Text("*Telefone") },
+                    isError = clientContactPhoneError.isNotEmpty(),
+                    modifier = Modifier
+                        .padding(8.dp),
+                )
+                if (clientContactPhoneError.isNotEmpty()) {
+                    Text(text = clientContactPhoneError, color = Color.Red)
                 }
-            )
-
-            OutlinedTextField(
-                value = clientEmail,
-                onValueChange = {
-                    clientEmail = it
-                },
-                modifier = Modifier
-                    .padding(8.dp),
-                label = {
-                    Text("Email")
+            }
+            Column {
+                OutlinedTextField(
+                    value = clientEmail,
+                    onValueChange = { clientEmail = it },
+                    label = { Text("Email") },
+                    isError = clientEmailErrorMessage.isNotEmpty(),
+                    modifier = Modifier
+                        .padding(8.dp),
+                )
+                if (clientEmailErrorMessage.isNotEmpty()) {
+                    Text(text = clientEmailErrorMessage, color = Color.Red)
                 }
-            )
+            }
         }
 
         HorizontalDividerWithText(text = "Documentos")
 
         Row {
-            OutlinedTextField(
-                value = cpf,
-                onValueChange = {
-                    cpf = it
-                },
-                modifier = Modifier
-                    .padding(8.dp),
-                label = {
-                    Text("*CPF")
+            Column {
+                OutlinedTextField(
+                    value = viewModel.cpf,
+                    onValueChange = { viewModel.cpf = it },
+                    label = { Text("*CPF") },
+                    isError = cpfErrorMessage.isNotEmpty(),
+                    modifier = Modifier
+                        .padding(8.dp),
+                    visualTransformation = CPFVisualTransformation(),
+                )
+                if (cpfErrorMessage.isNotEmpty()) {
+                    Text(text = cpfErrorMessage, color = Color.Red)
                 }
-            )
+            }
 
-            OutlinedTextField(
-                value = rg,
-                onValueChange = {
-                    rg = it
-                },
-                modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth(),
-                label = {
-                    Text("*RG")
+            Column {
+                OutlinedTextField(
+                    value = viewModel.rg,
+                    onValueChange = { viewModel.rg = it },
+                    label = { Text("*RG") },
+                    isError = rgErrorMessage.isNotEmpty(),
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth()
+                )
+                if (rgErrorMessage.isNotEmpty()) {
+                    Text(text = rgErrorMessage, color = Color.Red)
                 }
-            )
+            }
         }
 
         Row {
-            OutlinedTextField(
-                value = orgaoEmissor,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
-                onValueChange = {
-                    orgaoEmissor = it
-                },
-                modifier = Modifier
-                    .padding(8.dp),
-                label = {
-                    Text("*Orgão Emissor")
+            Column {
+                OutlinedTextField(
+                    value = viewModel.orgaoEmissor,
+                    onValueChange = { viewModel.orgaoEmissor = it },
+                    label = { Text("*Orgão Emissor") },
+                    modifier = Modifier
+                        .padding(8.dp),
+                )
+                if (orgaoEmissorErrorMessage.isNotEmpty()) {
+                    Text(text = orgaoEmissorErrorMessage, color = Color.Red)
                 }
-            )
+            }
         }
 
         Row(
@@ -268,45 +301,45 @@ fun ClientInfoForm(
                     disabledContainerColor = MaterialTheme.colorScheme.secondary,
                     disabledContentColor = Color.White,
                 ),
-                onClick = {
-                    Log.i("DocumentsUploadForm", "DocumentsUploadForm: testando")
-                    back()
-                },
+                onClick = { back() },
                 modifier = Modifier
                     .width(width = 110.dp)
                     .height(height = 46.dp),
             ) {
-                Text(
-                    text = "Voltar",
-                )
+                Text(text = "Voltar")
             }
             Spacer(modifier = Modifier.width(10.dp))
             Button(
                 onClick = {
-                    Log.i("DocumentsUploadForm", "DocumentsUploadForm: testando")
-                    next()
+                    val result = viewModel.validateForm()
+                    when (result) {
+                        is ValidationResult.Success -> next()
+                        is ValidationResult.Error -> {
+                        }
+                    }
                 },
                 modifier = Modifier
                     .width(width = 110.dp)
                     .height(height = 46.dp),
             ) {
-                Text(
-                    text = "Próximo",
-                )
+                Text(text = "Próximo")
             }
         }
     }
 }
+
+
 
 fun Long.toBrazilianDate(): String {
     val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale("pt-br"))
     return dateFormat.format(Date(this))
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview
 @Composable
 fun ClientInfoFormPreview() {
     VendedorcadastroclienteTheme {
-        ClientInfoForm({}, {})
+        ClientInfoForm(TODO(),{}, {})
     }
 }
